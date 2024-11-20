1b24a6dacfc745f31e671b63a5a9dd50be96c68c
BrandonSTran <tran.brandon0502@gmail.com>
2024-11-20 13:59:47 -0800

    Update pathcalc.cpp

--- Week6/pathcalc.cpp
+++ Week6/pathcalc.cpp
@@ -1,1 +1,11 @@
 
+int dTable[vCount];
+
+for(i = 0; i < vCount; i++) {
+   dTable[i] = std::numeric_limits<double>::infinity();
+}
+dTable[source] = 0;
+
+
+//predecessor table holds value of previous node
+int predecessor[vCount];
